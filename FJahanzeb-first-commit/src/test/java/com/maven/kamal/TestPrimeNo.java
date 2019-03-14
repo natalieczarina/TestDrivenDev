896c18af597efbc3d0d71fda14af0fd1b1d5f48e
faizajahanzeb <47185146+faizajahanzeb@users.noreply.github.com>
2019-03-14 09:56:30 -0400

    Add files via upload

--- FJahanzeb-first-commit/src/test/java/com/maven/kamal/TestPrimeNo.java
+++ FJahanzeb-first-commit/src/test/java/com/maven/kamal/TestPrimeNo.java
@@ -20,7 +20,7 @@ public class TestPrimeNo {
 	//@After
 	@Test
 	public void testPrimeNo() {
-		assertEquals(true, PrimeNo.checkPrime(20));
+		assertEquals(true, PrimeNo.checkPrime(19));
 	
 	}
 	//@Before
